045560d09fb4d4381be869d925f8af9d65d6e8d2
Andreas Tielitz <tielitz@thr3.de>
2016-11-07 11:10:16 +0100

    forgot the new files

---
+++ components/second-component/second-component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+var SecondComponent = React.createClass({
+  displayName: 'SecondComponent',
+  propTypes: {
+    name: React.PropTypes.string
+  },
+  sayGreeting: function () {
+    return 'Hello';
+  },
+  render: function() {
+    return (
+        <div>
+          {this.sayGreeting()}, {this.props.name}!
+        </div>
+      );
+  }
+});
